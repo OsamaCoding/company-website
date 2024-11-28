de17c4c5a84771cfcdc125c832a575d0604af8dc
OsamaCoding <osama1122335500@gmail.com>
2024-11-28 09:51:34 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/js/index.js",
+
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "js/bundle.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(sass|css|scss)$/,
+
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]",
+        },
+      },
+
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+
+  devServer: {
+    static: { directory: path.join(__dirname, "build") },
+    compress: true,
+    port: 9000,
+    open: true,
+    hot: true,
+    devMiddleware: {
+      writeToDisk: true,
+      stats: "errors-only",
+    },
+    watchFiles: ["src/**/*"],
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+      new CssMinimizerPlugin(),
+    ],
+  },
+
+  performance: {
+    hints: false, // تعطيل جميع تحذيرات الأداء
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./src/projects.html",
+      filename: "projects.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./src/project-details.html",
+      filename: "project-details.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./src/blog.html",
+      filename: "blog.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./src/blog-details.html",
+      filename: "blog-details.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./src/add-blog.html",
+      filename: "add-blog.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./src/about.html",
+      filename: "about.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./src/contact.html",
+      filename: "contact.html",
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
+    }),
+  ],
+};
